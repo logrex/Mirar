db6ebd2a9e7714bfaaa5a587b72d8450906a454b
logrex <arth.sharma2010@gmail.com>
2019-08-21 18:46:12 +0530

    Token

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const path = require('path')
+var cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+//JSON object to be added to cookie 
+let token = {
+  EHR: Math.random() * 100000000000
+};
+
+var app = express();
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+  // res.render('pages/index')
+  res.cookie("EHR", token);
+  res.send('EHR data added to cookie');
+})
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
